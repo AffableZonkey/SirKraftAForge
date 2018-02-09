4b2721c4cf0181d115d69d2ba942c88dbab8fc67
affablezonkey <zachvatwork@gmail.com>
2018-02-08 22:25:13 -0500

    worked on modget.lua - has errors

--- modget.lua
+++ modget.lua
@@ -5,17 +5,31 @@
 	
 	requests = require "requests"
 	
+	-- This function should get get 1 or 2 arguements
+	-- The mandatory arg is the modtype which is tacked on to the end of the url
+	-- optional arguement is the url itself
+	-- #TODO implement as tail call
 	function getMods(modtype)
 		curseF = "https://www.curseforge.com/minecraft/"
 		local modgetUrl = curseF..modtype
 		result = requests.get(modgetUrl)
-		print(result.text)
+		return result.text
 	end
-	
-	getMods("bukkit-plugins")
 	
 	--[[ TODO :
 		-- parse html
-		-- use tail call or recursion to switch between mods and other areas
 		-- sanitize stuff to print in screen for selection
+		-- interactions with main to list out mods
 		--]]
+		
+	function parseModhtml(modhtmltxt)
+		local parser = require "htmlparser"
+		parsedmodtxt = parser.parse(modhtmltxt)
+		local mhtelements = root(".class:[attribute*='pd-x-1']")
+		for _, elemnt in ipairs(mhtelements) do
+			print(elemnt.name)
+		end
+	end
+		
+	mods = getMods("bukkit-plugins")
+	parseModhtml(mods)
